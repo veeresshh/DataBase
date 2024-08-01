f2f762dde1fb018bbd215af5f83b7835f69313d0
veeresshh <veeresh0270@gmail.com>
2024-08-01 12:27:24 +0530

    First Commit

---
+++ src/Database.java
@@ -0,0 +1,44 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Database {
+
+public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+	
+      String host="localhost";
+
+      String port= "3306";
+
+      Connection con=DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/Jyothi", "root", "Jyothi@2005");
+
+      Statement s=con.createStatement();
+
+      ResultSet rs=s.executeQuery("select * from UserDetails where Name = 'Ram';");
+      
+      
+      while(rs.next())
+
+      {
+
+      WebDriver Driver = new ChromeDriver();
+
+      Driver.get("https://login.salesforce.com");
+
+      Driver.findElement(By.xpath(".//*[@id='username']")).sendKeys(rs.getString("Username"));
+
+      Driver.findElement(By.xpath(".//*[@id='password']")).sendKeys(rs.getString("Password"));
+  
+
+      }
+
+	}
+
+}
+
